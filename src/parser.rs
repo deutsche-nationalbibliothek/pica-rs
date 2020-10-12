e0fe45338676c73f95c3bcf7ef6ac5b6061250e6
Nico Wagner <nwagner84@protonmail.com>
2020-10-12 16:41:00 +0200

    Add occurrence parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,16 @@
 use nom::IResult;
 
 use nom::{
-    character::complete::one_of, combinator::recognize, multi::count,
-    sequence::tuple,
+    character::complete::one_of,
+    combinator::recognize,
+    multi::count,
+    sequence::{preceded, tuple},
 };
 
 /// Parse a Pica+ tag.
 ///
 /// A Pica+ tag starts with a digit less than three followed by two digits and
-/// an uppercase letter or an '@' character. If the parses succeeds, the
+/// an uppercase letter or an '@' character. If the parser succeeds, the
 /// remaining input and the tag is returned as an tuple wrapped in an [`Ok`].
 ///
 /// # Example
@@ -26,6 +28,26 @@ pub fn parse_tag(i: &str) -> IResult<&str, &str> {
     )))(i)
 }
 
+/// Parse the field occurrence.
+///
+/// The field occurrence is preceded by one '/' character followed by two
+/// digits. If the parser succeeds, the remaining input and the occurrence is
+/// returned as an tuple wrapped in an [`Ok`].
+///
+/// # Example
+/// ```
+/// use pica::parser::parse_occurrence;
+///
+/// let (_, occ) = parse_occurrence("/00").unwrap();
+/// assert_eq!(occ, "00");
+/// ```
+pub fn parse_occurrence(i: &str) -> IResult<&str, &str> {
+    preceded(
+        nom::character::complete::char('/'),
+        recognize(count(one_of("0123456789"), 2)),
+    )(i)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +59,12 @@ mod tests {
         assert!(parse_tag("0A0A").is_err());
         assert!(parse_tag("00AA").is_err());
         assert!(parse_tag("0000").is_err());
+    }
+
+    #[test]
+    fn test_parse_occurrence() {
+        assert_eq!(parse_occurrence("/00"), Ok(("", "00")));
+        assert_eq!(parse_occurrence("/01"), Ok(("", "01")));
+        assert!(parse_occurrence("00").is_err());
     }
 }
