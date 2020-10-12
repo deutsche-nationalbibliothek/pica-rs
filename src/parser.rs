fee1e796e301d5585016241b86a13af31b484b69
Nico Wagner <nwagner84@protonmail.com>
2020-10-12 17:27:05 +0200

    Add subfield parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,18 @@
-use nom::IResult;
-
 use nom::{
-    character::complete::one_of,
-    combinator::recognize,
-    multi::count,
-    sequence::{preceded, tuple},
+    branch::alt,
+    character::complete::{none_of, one_of},
+    combinator::{map, opt, recognize},
+    multi::{count, many1},
+    sequence::{pair, preceded, tuple},
+    IResult,
 };
+use std::iter::FromIterator;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Subfield {
+    pub code: char,
+    pub value: Option<String>,
+}
 
 /// Parse a Pica+ tag.
 ///
@@ -48,6 +55,40 @@ pub fn parse_occurrence(i: &str) -> IResult<&str, &str> {
     )(i)
 }
 
+/// Parse a subfield.
+///
+/// A subfield starts with the unit separator (\x1f) followed by the subfield
+/// code (alpha numerical character). The optional subfield value ends with a
+/// unit separator or an record separator (\x1f). If the parse succeeds the
+/// remaining input and the parsed [`Subfield`] is returned as an tuple wrapped
+/// in an [`Ok`].
+///
+/// # Example
+///
+/// ```
+/// use pica::parser::parse_subfield;
+///
+/// let (_, subfield) = parse_subfield("\x1fa123456789").unwrap();
+/// assert_eq!(subfield.code, 'a');
+/// assert_eq!(subfield.value, Some("123456789".to_string()));
+/// ```
+pub fn parse_subfield(i: &str) -> IResult<&str, Subfield> {
+    preceded(
+        nom::character::complete::char('\x1f'),
+        map(
+            pair(
+                alt((
+                    one_of("ABCdefghijklmnopqrstuvwxyz"),
+                    one_of("abcdefghijklmnopqrstuvwxyz"),
+                    one_of("0123456789"),
+                )),
+                opt(map(many1(none_of("\x1f\x1e")), |s| String::from_iter(s))),
+            ),
+            |(c, v)| Subfield { code: c, value: v },
+        ),
+    )(i)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +107,30 @@ mod tests {
         assert_eq!(parse_occurrence("/00"), Ok(("", "00")));
         assert_eq!(parse_occurrence("/01"), Ok(("", "01")));
         assert!(parse_occurrence("00").is_err());
+    }
+
+    #[test]
+    fn test_parse_subfield() {
+        assert_eq!(
+            parse_subfield("\x1fa123456789"),
+            Ok((
+                "",
+                Subfield {
+                    code: 'a',
+                    value: Some("123456789".to_string())
+                }
+            ))
+        );
+
+        assert_eq!(
+            parse_subfield("\x1fa"),
+            Ok((
+                "",
+                Subfield {
+                    code: 'a',
+                    value: None,
+                }
+            ))
+        );
     }
 }
