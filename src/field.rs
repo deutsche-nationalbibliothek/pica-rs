c6261b7d9cb34d2a538f2991595fa2b9336f1fc9
Nico Wagner <nwagner84@protonmail.com>
2020-10-29 19:14:38 +0100

    Implement `Display` trait for field (#34)

--- src/field.rs
+++ src/field.rs
@@ -1,4 +1,5 @@
 use crate::Subfield;
+use std::fmt;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Field {
@@ -29,6 +30,48 @@ impl Field {
     }
 }
 
+impl fmt::Display for Field {
+    /// Format a field.
+    ///
+    /// # Example
+    /// ```
+    /// use pica::{Field, Subfield};
+    ///
+    /// let field = Field::new(
+    ///     "012A",
+    ///     None,
+    ///     vec![
+    ///         Subfield::new('a', "123"),
+    ///         Subfield::new('b', "456"),
+    ///         Subfield::new('c', "789"),
+    ///     ],
+    /// );
+    ///
+    /// assert_eq!(field.to_string(), "012A $a 123 $b 456 $c 789");
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.tag)?;
+
+        if let Some(ref occurrence) = self.occurrence {
+            write!(f, "/{}", occurrence)?;
+        }
+
+        if !self.subfields.is_empty() {
+            write!(
+                f,
+                " {}",
+                self.subfields
+                    .iter()
+                    .map(|s| format!("{}", s))
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,5 +88,25 @@ mod tests {
         assert_eq!(field.tag, "003@");
         assert_eq!(field.occurrence, Some("".to_string()));
         assert!(field.subfields.is_empty());
+    }
+
+    #[test]
+    fn test_fmt() {
+        let field = Field::new("012A", None, vec![]);
+        assert_eq!(format!("{}", field), "012A");
+
+        let field = Field::new("012A", Some("01"), vec![]);
+        assert_eq!(format!("{}", field), "012A/01");
+
+        let field =
+            Field::new("012A", Some("00"), vec![Subfield::new('a', "123")]);
+        assert_eq!(format!("{}", field), "012A/00 $a 123");
+
+        let field = Field::new(
+            "012A",
+            None,
+            vec![Subfield::new('a', "123"), Subfield::new('b', "456")],
+        );
+        assert_eq!(format!("{}", field), "012A $a 123 $b 456");
     }
 }
