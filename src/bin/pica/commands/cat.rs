1fa332ae6431eb63e56bb7fc2f3c6f897234f1f5
Nico Wagner <nwagner84@protonmail.com>
2020-11-05 11:31:09 +0100

    Add cat command

---
+++ src/bin/pica/commands/cat.rs
@@ -0,0 +1,53 @@
+use crate::util::{App, CliArgs, CliError, CliResult};
+use clap::{Arg, SubCommand};
+use pica::Record;
+use std::boxed::Box;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use std::str::FromStr;
+
+pub fn cli() -> App {
+    SubCommand::with_name("cat")
+        .about("Concatenate records from multiple files.")
+        .arg(
+            Arg::with_name("skip-invalid")
+                .short("s")
+                .long("skip-invalid")
+                .help("skip invalid records"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("--output")
+                .value_name("file")
+                .help("Write output to <file> instead of stdout."),
+        )
+        .arg(Arg::with_name("filenames").multiple(true).required(true))
+}
+
+pub fn run(args: &CliArgs) -> CliResult<()> {
+    let mut writer: Box<dyn Write> = match args.value_of("output") {
+        None => Box::new(io::stdout()),
+        Some(filename) => Box::new(File::create(filename)?),
+    };
+
+    for filename in args.values_of("filenames").unwrap() {
+        let reader = BufReader::new(File::open(filename)?);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if let Ok(_record) = Record::from_str(&line) {
+                writer.write_all(line.as_bytes())?;
+                writer.write_all(b"\n")?;
+            } else if !args.is_present("skip-invalid") {
+                return Err(CliError::Other(format!(
+                    "could not read record: {}",
+                    line
+                )));
+            }
+        }
+    }
+
+    writer.flush()?;
+    Ok(())
+}
