710a2d34653732597d022bd35c47b0d7222f992b
Nico Wagner <nwagner84@protonmail.com>
2020-11-05 09:36:06 +0100

    Add expr unit tests

--- src/expr.rs
+++ src/expr.rs
@@ -33,3 +33,97 @@ impl FromStr for Expr {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expr_from_str() {
+        let expr: Expr = "003@.0 == 123456789X".parse().unwrap();
+        assert_eq!(
+            expr,
+            Expr::Predicate(
+                Path::new("003@", "", '0'),
+                ComparisonOp::Eq,
+                "123456789X".to_string()
+            )
+        );
+
+        let expr: Expr = "003@.0 != 123456789X".parse().unwrap();
+        assert_eq!(
+            expr,
+            Expr::Predicate(
+                Path::new("003@", "", '0'),
+                ComparisonOp::Ne,
+                "123456789X".to_string()
+            )
+        );
+
+        let expr: Expr = "003@.0 == 123456789X && 012A.a == a".parse().unwrap();
+        let lhs = Expr::Predicate(
+            Path::new("003@", "", '0'),
+            ComparisonOp::Eq,
+            "123456789X".to_string(),
+        );
+        let rhs = Expr::Predicate(
+            Path::new("012A", "", 'a'),
+            ComparisonOp::Eq,
+            "a".to_string(),
+        );
+
+        assert_eq!(
+            expr,
+            Expr::Connective(Box::new(lhs), LogicalOp::And, Box::new(rhs))
+        );
+
+        let expr: Expr = "003@.0 == 123456789X || 012A.a == a".parse().unwrap();
+        let lhs = Expr::Predicate(
+            Path::new("003@", "", '0'),
+            ComparisonOp::Eq,
+            "123456789X".to_string(),
+        );
+        let rhs = Expr::Predicate(
+            Path::new("012A", "", 'a'),
+            ComparisonOp::Eq,
+            "a".to_string(),
+        );
+
+        assert_eq!(
+            expr,
+            Expr::Connective(Box::new(lhs), LogicalOp::Or, Box::new(rhs))
+        );
+
+        let expr: Expr = "(003@.0 == 123456789X && 012A.a == a) || 012A.a == b"
+            .parse()
+            .unwrap();
+        let p1 = Expr::Predicate(
+            Path::new("003@", "", '0'),
+            ComparisonOp::Eq,
+            "123456789X".to_string(),
+        );
+        let p2 = Expr::Predicate(
+            Path::new("012A", "", 'a'),
+            ComparisonOp::Eq,
+            "a".to_string(),
+        );
+        let p3 = Expr::Predicate(
+            Path::new("012A", "", 'a'),
+            ComparisonOp::Eq,
+            "b".to_string(),
+        );
+
+        assert_eq!(
+            expr,
+            Expr::Connective(
+                Box::new(Expr::Parens(Box::new(Expr::Connective(
+                    Box::new(p1),
+                    LogicalOp::And,
+                    Box::new(p2)
+                )))),
+                LogicalOp::Or,
+                Box::new(p3)
+            )
+        );
+    }
+}
