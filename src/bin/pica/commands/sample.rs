51c039e5e2a9ed91f43943dfc492ed2aa360a1af
Nico Wagner <nwagner84@protonmail.com>
2020-11-06 09:21:08 +0100

    Improve error message for invalid sample size

--- src/bin/pica/commands/sample.rs
+++ src/bin/pica/commands/sample.rs
@@ -32,26 +32,29 @@ pub fn run(args: &CliArgs) -> CliResult<()> {
     let reader = config.reader(args.value_of("filename"))?;
     let skip_invalid = args.is_present("skip-invalid");
 
-    let sample_size =
-        match args.value_of("sample-size").unwrap().parse::<usize>() {
-            Ok(v) => v,
-            Err(_) => {
-                return Err(CliError::Other("invalid sample size".to_string()))
-            }
-        };
+    let sample_size = args.value_of("sample-size").unwrap();
+    let n = match sample_size.parse::<usize>() {
+        Ok(v) => v,
+        Err(_) => {
+            return Err(CliError::Other(format!(
+                "invalid sample size '{}'. expected usize.",
+                sample_size
+            )));
+        }
+    };
 
-    let mut reservoir: Vec<String> = Vec::with_capacity(sample_size);
+    let mut reservoir: Vec<String> = Vec::with_capacity(n);
     let mut rng = thread_rng();
 
     for (i, line) in reader.lines().enumerate() {
         let line = line.unwrap();
 
         if let Ok(_record) = Record::from_str(&line) {
-            if i < sample_size {
+            if i < n {
                 reservoir.push(line);
             } else {
                 let j = rng.gen_range(0, i);
-                if j < sample_size {
+                if j < n {
                     reservoir[j] = line;
                 }
             }
