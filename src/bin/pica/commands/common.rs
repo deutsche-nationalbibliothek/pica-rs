442a3505daf94eb4b421740c43f813514c3912c9
Nico Wagner <nwagner84@protonmail.com>
2020-11-17 14:18:58 +0100

    Support gzip compressed files
    Closes #61

--- src/bin/pica/commands/common.rs
+++ src/bin/pica/commands/common.rs
@@ -1,5 +1,8 @@
+use flate2::read::GzDecoder;
+use std::ffi::OsStr;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Write};
+use std::io::{self, BufRead, BufReader, Read, Write};
+use std::path::Path;
 
 #[derive(Debug)]
 pub struct Config;
@@ -15,7 +18,18 @@ impl Config {
     ) -> io::Result<Box<dyn BufRead>> {
         Ok(match filename {
             None => Box::new(BufReader::new(io::stdin())),
-            Some(filename) => Box::new(BufReader::new(File::open(filename)?)),
+            Some(filename) => {
+                let path = Path::new(filename);
+
+                let reader: Box<dyn Read> =
+                    if path.extension() == Some(OsStr::new("gz")) {
+                        Box::new(GzDecoder::new(File::open(path)?))
+                    } else {
+                        Box::new(File::open(path)?)
+                    };
+
+                Box::new(BufReader::new(reader))
+            }
         })
     }
 
