cf386e18c8baef228f690441432bda747ba7f929
Nico Wagner <nwagner84@protonmail.com>
2020-12-02 08:34:05 +0100

    Move path related parsers into path module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate nom;
 extern crate serde;
 extern crate serde_json;
 
+pub use path::{parse_path, parse_path_list, Path};
+
 mod error;
 mod field;
 mod filter;
@@ -10,10 +12,10 @@ mod path;
 mod record;
 mod string;
 mod subfield;
+mod utils;
 
 pub use error::ParsePicaError;
 pub use field::Field;
 pub use filter::Filter;
-pub use path::Path;
 pub use record::Record;
 pub use subfield::Subfield;
