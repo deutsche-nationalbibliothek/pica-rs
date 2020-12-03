7b6c3776a35fcf683b32096fb520de9309cc9ee3
Nico Wagner <nwagner84@protonmail.com>
2020-12-03 21:12:14 +0100

    Introduce new select syntax

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub use self::field::Field;
 pub use self::filter::Filter;
 pub use self::path::{parse_path, parse_path_list, Path};
 pub use self::record::Record;
+pub use self::select::{Selector, Selectors};
 pub use self::subfield::Subfield;
 
 mod error;
@@ -14,6 +15,7 @@ mod field;
 mod filter;
 mod path;
 mod record;
+mod select;
 mod string;
 mod subfield;
 mod utils;
