d1625be8877adedf28882cf559f2586a9c00087c
Nico Wagner <nwagner84@protonmail.com>
2021-01-08 09:14:59 +0100

    Add field, path and record tests

---
+++ tests/test_path.rs
@@ -0,0 +1,48 @@
+extern crate pica;
+
+use pica::{ParsePicaError, Path};
+
+#[test]
+fn path_new() {
+    let path = Path::new("012A", Some("00"), '0', Some(0));
+    assert_eq!(path.tag(), "012A");
+    assert_eq!(path.occurrence(), Some("00"));
+    assert_eq!(path.name(), '0');
+    assert_eq!(path.index(), Some(0));
+}
+
+#[test]
+fn path_to_string() {
+    let path = Path::new("012A", Some("00"), '0', Some(1));
+    assert_eq!(path.to_string(), "012A/00.0[1]");
+
+    let path = Path::new("012A", None, '0', Some(1));
+    assert_eq!(path.to_string(), "012A.0[1]");
+
+    let path = Path::new("012A", None, '0', None);
+    assert_eq!(path.to_string(), "012A.0");
+}
+
+#[test]
+fn path_from_str() {
+    let path = " 012A/00.0[1] ".parse::<Path>().unwrap();
+    assert_eq!(path.tag(), "012A");
+    assert_eq!(path.occurrence(), Some("00"));
+    assert_eq!(path.name(), '0');
+    assert_eq!(path.index(), Some(1));
+
+    let path = "012A/00.0".parse::<Path>().unwrap();
+    assert_eq!(path.tag(), "012A");
+    assert_eq!(path.occurrence(), Some("00"));
+    assert_eq!(path.name(), '0');
+    assert_eq!(path.index(), None);
+
+    let path = "012A.0".parse::<Path>().unwrap();
+    assert_eq!(path.tag(), "012A");
+    assert_eq!(path.occurrence(), None);
+    assert_eq!(path.name(), '0');
+    assert_eq!(path.index(), None);
+
+    let path = "012A".parse::<Path>();
+    assert_eq!(path, Err(ParsePicaError::InvalidPath));
+}
---
+++ tests/test_record.rs
@@ -0,0 +1,95 @@
+extern crate pica;
+
+use pica::{Field, ParsePicaError, Path, Record, Subfield};
+
+#[test]
+fn record_new() {
+    let field =
+        Field::new("003@", None, vec![Subfield::new('0', "123").unwrap()]);
+
+    let record = Record::new(vec![field]);
+    assert_eq!(record.len(), 1);
+}
+
+#[test]
+fn record_len() {
+    let field1 =
+        Field::new("003@", None, vec![Subfield::new('0', "123").unwrap()]);
+    let field2 =
+        Field::new("002@", None, vec![Subfield::new('0', "Tp1").unwrap()]);
+
+    let record = Record::new(vec![field1, field2]);
+    assert_eq!(record.len(), 2);
+}
+
+#[test]
+fn record_is_empty() {
+    let field =
+        Field::new("003@", None, vec![Subfield::new('0', "123").unwrap()]);
+
+    let record = Record::new(vec![field]);
+    assert!(!record.is_empty());
+
+    let record = Record::new(vec![]);
+    assert!(record.is_empty());
+}
+
+#[test]
+fn record_pretty() {
+    let record = Record::new(vec![
+        Field::new(
+            "003@",
+            None,
+            vec![Subfield::new('0', "123456789").unwrap()],
+        ),
+        Field::new(
+            "012A",
+            Some("00"),
+            vec![
+                Subfield::new('a', "123").unwrap(),
+                Subfield::new('b', "456").unwrap(),
+            ],
+        ),
+    ]);
+
+    assert_eq!(record.pretty(), "003@ $0 123456789\n012A/00 $a 123 $b 456");
+}
+
+#[test]
+fn record_path() {
+    let record = Record::decode(
+        "012A \u{1f}a1\u{1f}a2\u{1f}b3\u{1e}012A \u{1f}a3\u{1e}",
+    )
+    .unwrap();
+
+    let path = "012A.a".parse::<Path>().unwrap();
+    assert_eq!(record.path(&path), vec!["1", "2", "3"]);
+
+    let path = "012A.a[1]".parse::<Path>().unwrap();
+    assert_eq!(record.path(&path), vec!["2"]);
+
+    let path = "012A.a[9]".parse::<Path>().unwrap();
+    assert!(record.path(&path).is_empty());
+}
+
+#[test]
+fn record_decode() {
+    let record: Record =
+        Record::decode("003@ \u{1f}0123\u{1e}012A/00 \u{1f}a123\u{1e}")
+            .unwrap();
+
+    let field =
+        Field::new("003@", None, vec![Subfield::new('0', "123").unwrap()]);
+    assert!(record.contains(&field));
+
+    let field = Field::new(
+        "012A",
+        Some("00"),
+        vec![Subfield::new('a', "123").unwrap()],
+    );
+    assert!(record.contains(&field));
+    assert_eq!(record.len(), 2);
+
+    let result = Record::decode("003@ \u{1f}0123\u{1e}012A/00 \u{1f}a123");
+    assert_eq!(result, Err(ParsePicaError::InvalidRecord));
+}
