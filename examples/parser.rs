6029b9bdf4db3e93f4532e12771876bcb03f1267
Nico Wagner <nwagner84@protonmail.com>
2021-01-08 15:27:04 +0100

    Add parser example (#139)

---
+++ examples/parser.rs
@@ -0,0 +1,23 @@
+extern crate pica;
+
+use pica::Record;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let filename = env::args()
+        .skip(1)
+        .next()
+        .unwrap_or("tests/data/1.dat".to_string());
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match Record::decode(&line.unwrap()) {
+            Ok(record) => println!("{}", record.pretty()),
+            Err(_) => eprintln!("invalid record!"),
+        }
+    }
+}
