f072ae26bdb05fafc5f63590113fe46013b92f6e
Nico Wagner <nwagner84@protonmail.com>
2021-01-08 10:46:59 +0100

    Add filter tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate serde_json;
 
 pub use self::error::ParsePicaError;
 pub use self::field::Field;
-pub use self::filter::Filter;
+pub use self::filter::{Filter, ParseFilterError};
 pub use self::path::{parse_path, parse_path_list, Path};
 pub use self::record::Record;
 pub use self::select::{Selector, Selectors};
