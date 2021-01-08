168a95b187151703a4bc5caa1a54ac7496144345
Nico Wagner <nwagner84@protonmail.com>
2021-01-08 09:35:37 +0100

    Update filter tests

--- tests/test_filter_cmd.rs
+++ tests/test_filter_cmd.rs
@@ -7,7 +7,7 @@ static SAMPLE2: &'static str = include_str!("data/2.dat");
 static SAMPLE3: &'static str = include_str!("data/3.dat");
 
 #[test]
-fn test_filter() {
+fn test_eq_filter() {
     let result = CliRunner::new().invoke(
         "filter",
         &[
@@ -19,12 +19,15 @@ fn test_filter() {
     assert!(result.status.success());
 
     assert_eq!(String::from_utf8(result.stdout).unwrap(), SAMPLE1);
+}
 
+#[test]
+fn test_ne_filter() {
     let result = CliRunner::new().invoke(
         "filter",
         &[
             "--skip-invalid",
-            "003@.0 == '123456789X' || 003@{0 == '234567891X'}",
+            "003@.0 != '123456789X'",
             "tests/data/all.dat.gz",
         ],
     );
@@ -32,8 +35,218 @@ fn test_filter() {
 
     assert_eq!(
         String::from_utf8(result.stdout).unwrap(),
-        format!("{}{}", SAMPLE1, SAMPLE2)
+        format!("{}{}", SAMPLE2, SAMPLE3)
     );
+}
+
+#[test]
+fn test_starts_with_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &["--skip-invalid", "003@.0 =^ '123'", "tests/data/all.dat.gz"],
+    );
+    assert!(result.status.success());
+
+    assert_eq!(String::from_utf8(result.stdout).unwrap(), SAMPLE1);
+}
+
+#[test]
+fn test_ends_with_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &["--skip-invalid", "002@.0 =$ 'p2'", "tests/data/all.dat.gz"],
+    );
+    assert!(result.status.success());
+
+    assert_eq!(String::from_utf8(result.stdout).unwrap(), SAMPLE2);
+}
+
+#[test]
+fn test_regex_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@.0 =~ '^Tp[12]$'",
+            "tests/data/all.dat.gz",
+        ],
+    );
+    assert!(result.status.success());
+
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}{}", SAMPLE1, SAMPLE2, SAMPLE3)
+    );
+}
+
+#[test]
+fn test_in_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@.0 in ['Tp1', 'Tp3']",
+            "tests/data/all.dat.gz",
+        ],
+    );
+    assert!(result.status.success());
+
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}", SAMPLE1, SAMPLE3)
+    );
+}
+
+#[test]
+fn test_or_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{0 == 'Tp1' || 0 == 'Tp3'}",
+            "tests/data/all.dat.gz",
+        ],
+    );
+    assert!(result.status.success());
+
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}", SAMPLE1, SAMPLE3)
+    );
+
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{0 == 'Tp1' || 0 == 'Tp3'} || 003@.0 == '234567891X'",
+            "tests/data/all.dat.gz",
+        ],
+    );
+    assert!(result.status.success());
+
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}{}", SAMPLE1, SAMPLE2, SAMPLE3)
+    );
+}
+
+#[test]
+fn test_and_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{0 =^ 'Tp' && 0 =$ '2'}",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(String::from_utf8(result.stdout).unwrap(), SAMPLE2);
+
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{0 =^ 'Tp' && 0 =$ '2'} && 003@.0 == '234567891X'",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(String::from_utf8(result.stdout).unwrap(), SAMPLE2);
+}
+
+#[test]
+fn test_grouped_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{ (0 =^ 'Tp' && 0 =$ '2') || 0 == 'Tp1' }",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}{}", SAMPLE1, SAMPLE2, SAMPLE3)
+    );
+
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "003@.0 =$ 'X' && (002@{0 =^ 'Tp' && 0 =$ '2'} || 002@.0 == 'Tp1')",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}{}", SAMPLE1, SAMPLE2, SAMPLE3)
+    );
+}
+
+#[test]
+fn test_not_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{!(0 == 'Tp2' || c?)}",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}", SAMPLE1, SAMPLE3)
+    );
+
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "!(002@.0 == 'Tp2' || 002@.0 == 'Tp3')",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}", SAMPLE1, SAMPLE3)
+    );
+}
+
+#[test]
+fn test_exists_filter() {
+    let result = CliRunner::new().invoke(
+        "filter",
+        &["--skip-invalid", "002@.0?", "tests/data/all.dat.gz"],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(
+        String::from_utf8(result.stdout).unwrap(),
+        format!("{}{}{}", SAMPLE1, SAMPLE2, SAMPLE3)
+    );
+
+    let result = CliRunner::new().invoke(
+        "filter",
+        &[
+            "--skip-invalid",
+            "002@{0? && 0 == 'Tp2'}",
+            "tests/data/all.dat.gz",
+        ],
+    );
+
+    assert!(result.status.success());
+    assert_eq!(String::from_utf8(result.stdout).unwrap(), SAMPLE2);
 }
 
 #[test]
