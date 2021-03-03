bfc9decfe8a3b5e661873fba6e929a5b4020f4d7
Nico Wagner <nwagner84@protonmail.com>
2021-03-03 13:39:13 +0100

    Refactor select (#156)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub use self::filter::{Filter, ParseFilterError};
 pub use self::path::{parse_path, Path};
-pub use self::record::legacy;
 pub use self::record::{Field, Occurrence, Record, Subfield};
 pub use self::select::{Outcome, Selector, Selectors};
 
