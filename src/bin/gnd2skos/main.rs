b8d52bc1090f644d6d79c84ef98204c9a6d8fb7c
Nico Wagner <nwagner84@protonmail.com>
2021-03-08 15:46:45 +0100

    Add gnd2skos skeleton

--- src/bin/gnd2skos/main.rs
+++ src/bin/gnd2skos/main.rs
@@ -1,3 +1,70 @@
-fn main() {
-    println!("gnd2skos")
+#[macro_use]
+extern crate clap;
+
+mod cli;
+mod utils;
+
+use bstr::io::BufReadExt;
+use flate2::read::GzDecoder;
+use pica::{Filter, Record};
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read, Write};
+use std::path::Path;
+
+use utils::{CliError, CliResult};
+
+fn main() -> CliResult<()> {
+    let args = cli::build_cli().get_matches();
+    let skip_invalid = args.is_present("skip-invalid");
+
+    let filter = match args.value_of("filter") {
+        None => Filter::True,
+        Some(filter_str) => match Filter::decode(filter_str) {
+            Ok(filter) => filter,
+            Err(_) => {
+                return Err(CliError::Other(format!(
+                    "invalid filter: {}",
+                    filter_str
+                )))
+            }
+        },
+    };
+
+    let reader: Box<dyn BufRead> = match args.value_of("filename") {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(filename) => {
+            let path = Path::new(filename);
+
+            let reader: Box<dyn Read> =
+                if path.extension() == Some(OsStr::new("gz")) {
+                    Box::new(GzDecoder::new(File::open(path)?))
+                } else {
+                    Box::new(File::open(path)?)
+                };
+
+            Box::new(BufReader::new(reader))
+        }
+    };
+
+    let writer: Box<dyn Write> = match args.value_of("output") {
+        Some(filename) => Box::new(File::create(filename)?),
+        None => Box::new(io::stdout()),
+    };
+
+    for result in reader.byte_lines() {
+        let line = result?;
+
+        if let Ok(_record) = Record::from_bytes(&line) {
+            println!("record = {:?}", _record);
+            break;
+        } else if !skip_invalid {
+            return Err(CliError::Other(format!(
+                "could not read record: {}",
+                String::from_utf8(line).unwrap()
+            )));
+        }
+    }
+
+    Ok(())
 }
---
+++ src/bin/gnd2skos/utils.rs
@@ -0,0 +1,24 @@
+use std::{convert::From, fmt, io};
+
+pub type CliResult<T> = Result<T, CliError>;
+
+#[derive(Debug)]
+pub enum CliError {
+    Io(io::Error),
+    Other(String),
+}
+
+impl fmt::Display for CliError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CliError::Io(ref e) => e.fmt(f),
+            CliError::Other(ref s) => f.write_str(&**s),
+        }
+    }
+}
+
+impl From<io::Error> for CliError {
+    fn from(err: io::Error) -> CliError {
+        CliError::Io(err)
+    }
+}
