4551ff858452cd53ade3f9c4163602cb1047ae97
Nico Wagner <nwagner84@protonmail.com>
2021-03-30 10:02:36 +0200

    Tabula rasa

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
-pub use self::filter::{Filter, ParseFilterError};
-pub use self::path::{parse_path, Path};
-pub use self::record::{Field, Occurrence, Record, Subfield};
-pub use self::select::{Outcome, Selector, Selectors};
 
-mod filter;
-mod path;
-mod record;
-mod select;
