12118d7fa012152d02398a351c6255fd1b69cf4a
Nico Wagner <nwagner84@protonmail.com>
2021-08-13 08:08:49 +0200

    Add `pica cat` integration tests, pt.2

---
+++ tests/common/mod.rs
@@ -0,0 +1,33 @@
+use assert_cmd::Command;
+use std::fs::File;
+use std::io::Write;
+use tempfile::{Builder, TempDir};
+
+pub type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+pub struct TestContext {
+    tempdir: TempDir,
+}
+
+impl TestContext {
+    pub fn new() -> Self {
+        let tempdir = Builder::new().tempdir().unwrap();
+        TestContext { tempdir }
+    }
+}
+
+pub trait CommandExt {
+    fn with_config(&mut self, ctx: &TestContext, content: &str) -> &mut Self;
+}
+
+impl CommandExt for Command {
+    fn with_config(&mut self, ctx: &TestContext, content: &str) -> &mut Self {
+        let filename = ctx.tempdir.path().join("Pica.toml");
+        let mut config = File::create(&filename).expect("create config file");
+        config.write_all(content.as_bytes()).expect("write config");
+        config.flush().expect("flush config");
+
+        self.arg("--config").arg(filename);
+        self
+    }
+}
--- tests/pica/cat.rs
+++ tests/pica/cat.rs
@@ -1,22 +1,29 @@
 use assert_cmd::Command;
+use predicates::prelude::*;
 use std::fs::read_to_string;
+use std::path::Path;
+
+use crate::common::{CommandExt, TestContext, TestResult};
 
 #[test]
-fn pica_cat_single_file() {
-    let mut cmd = Command::cargo_bin("pica").unwrap();
+fn pica_cat_single_file() -> TestResult {
+    let mut cmd = Command::cargo_bin("pica")?;
     let assert = cmd
         .arg("cat")
         .arg("--skip-invalid")
         .arg("tests/data/1004916019.dat")
         .assert();
 
-    let expected = read_to_string("tests/data/1004916019.dat").unwrap();
+    let expected =
+        predicate::path::eq_file(Path::new("tests/data/1004916019.dat"));
     assert.success().stdout(expected);
+
+    Ok(())
 }
 
 #[test]
-fn pica_cat_multiple_file() {
-    let mut cmd = Command::cargo_bin("pica").unwrap();
+fn pica_cat_multiple_file() -> TestResult {
+    let mut cmd = Command::cargo_bin("pica")?;
     let assert = cmd
         .arg("cat")
         .arg("--skip-invalid")
@@ -30,4 +37,126 @@ fn pica_cat_multiple_file() {
         read_to_string("tests/data/000009229.dat").unwrap()
     );
     assert.success().stdout(expected);
+
+    Ok(())
+}
+
+#[test]
+fn pica_cat_gzip_file() -> TestResult {
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("cat")
+        .arg("--skip-invalid")
+        .arg("tests/data/1004916019.dat.gz")
+        .assert();
+
+    let expected =
+        predicate::path::eq_file(Path::new("tests/data/1004916019.dat"));
+    assert.success().stdout(expected);
+
+    Ok(())
+}
+
+#[test]
+fn pica_cat_skip_invalid() -> TestResult {
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("cat")
+        .arg("--skip-invalid")
+        .arg("tests/data/invalid.dat")
+        .assert();
+
+    assert.success().stdout(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd.arg("cat").arg("tests/data/dump.dat.gz").assert();
+    assert
+        .failure()
+        .code(1)
+        .stdout(predicate::path::eq_file(Path::new(
+            "tests/data/1004916019.dat",
+        )))
+        .stderr(predicate::eq("Pica Error: Invalid record on line 2.\n"));
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .with_config(
+            &TestContext::new(),
+            r#"[cat]
+skip-invalid = true
+"#,
+        )
+        .arg("cat")
+        .arg("tests/data/invalid.dat")
+        .assert();
+
+    assert.success().stdout(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .with_config(
+            &TestContext::new(),
+            r#"[global]
+skip-invalid = true
+"#,
+        )
+        .arg("cat")
+        .arg("tests/data/invalid.dat")
+        .assert();
+
+    assert.success().stdout(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .with_config(
+            &TestContext::new(),
+            r#"[global]
+skip-invalid = false
+
+[cat]
+skip-invalid = true
+"#,
+        )
+        .arg("cat")
+        .arg("tests/data/invalid.dat")
+        .assert();
+
+    assert.success().stdout(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .with_config(
+            &TestContext::new(),
+            r#"[global]
+skip-invalid = false
+
+[cat]
+skip-invalid = false
+"#,
+        )
+        .arg("cat")
+        .arg("--skip-invalid")
+        .arg("tests/data/invalid.dat")
+        .assert();
+
+    assert.success().stdout(predicate::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn pica_cat_missing_file() -> TestResult {
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("cat")
+        .arg("--skip-invalid")
+        .arg("tests/data/10049160XX.dat")
+        .assert();
+
+    assert
+        .failure()
+        .stderr("Pica Error: No such file or directory (os error 2)\n")
+        .stdout(predicate::str::is_empty())
+        .code(1);
+    Ok(())
 }
