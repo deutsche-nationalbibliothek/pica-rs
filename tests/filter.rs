2194f181a4355ec772fccf9dd250d2880fad572b
Nico Wagner <nwagner84@protonmail.com>
2021-09-02 19:41:33 +0200

    Add `OccurrenceMatcher` tests

---
+++ tests/filter.rs
@@ -0,0 +1,31 @@
+use pica::{Occurrence, OccurrenceMatcher};
+
+#[test]
+fn test_occurrence_matcher() {
+    // OccurrenceMatcher::Any
+    assert_eq!(Some(Occurrence::new("01").unwrap()), OccurrenceMatcher::Any);
+    assert_eq!(None, OccurrenceMatcher::Any);
+
+    // OccurrenceMatcher::None
+    assert_eq!(None, OccurrenceMatcher::None);
+    assert_ne!(
+        Some(Occurrence::new("01").unwrap()),
+        OccurrenceMatcher::None
+    );
+
+    // OccurrenceMatcher::Occurrence
+    let matcher = OccurrenceMatcher::Occurrence(Occurrence::new("01").unwrap());
+    assert_eq!(Some(Occurrence::new("01").unwrap()), matcher);
+    assert_ne!(Some(Occurrence::new("02").unwrap()), matcher);
+    assert_ne!(None, matcher);
+
+    // OccurrenceMatcher::Range
+    let min = Occurrence::new("02").unwrap();
+    let max = Occurrence::new("04").unwrap();
+    let matcher = OccurrenceMatcher::Range(min, max);
+    assert_ne!(Some(Occurrence::new("01").unwrap()), matcher);
+    assert_eq!(Some(Occurrence::new("02").unwrap()), matcher);
+    assert_eq!(Some(Occurrence::new("03").unwrap()), matcher);
+    assert_eq!(Some(Occurrence::new("04").unwrap()), matcher);
+    assert_ne!(Some(Occurrence::new("05").unwrap()), matcher);
+}
--- tests/main.rs
+++ tests/main.rs
@@ -1,2 +1,5 @@
+pub use self::common::TestResult;
+
 mod common;
+mod filter;
 mod pica;
