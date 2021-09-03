8923f0c0dc0b5ed7a54dd72d56e4b484a3286c60
Nico Wagner <nwagner84@protonmail.com>
2021-09-03 19:48:12 +0200

    add subfield validate test

--- tests/record.rs
+++ tests/record.rs
@@ -21,6 +21,20 @@ fn subfield_new() {
 }
 
 #[test]
+fn subfield_validate() {
+    assert_eq!(
+        format!(
+            "{}",
+            Subfield::new('0', vec![0, 159])
+                .unwrap()
+                .validate()
+                .unwrap_err()
+        ),
+        "invalid utf-8 sequence of 1 bytes from index 1"
+    );
+}
+
+#[test]
 fn occurrence_new() {
     assert!(Occurrence::new("01").is_ok());
     assert!(Occurrence::new("001").is_ok());
