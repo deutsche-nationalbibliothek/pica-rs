527e2575d1d2892415bca01d46cbf65f22b3c6c4
Nico Wagner <nwagner84@protonmail.com>
2021-09-03 16:12:29 +0200

    Latest

--- tests/record.rs
+++ tests/record.rs
@@ -1,9 +1,47 @@
-use pica::Subfield;
+use pica::{Field, Occurrence, Subfield};
 
 #[test]
 fn subfield_new() {
     assert!(Subfield::new('0', "12283643X").is_ok());
-    assert!(Subfield::new('!', "12283643X").is_err());
-    assert!(Subfield::new('a', "123\x1f34").is_err());
-    assert!(Subfield::new('a', "123\x1e34").is_err());
+
+    assert_eq!(
+        format!("{}", Subfield::new('!', "12283643X").unwrap_err()),
+        "Invalid subfield code '!'"
+    );
+
+    assert_eq!(
+        format!("{}", Subfield::new('0', "12283\x1e643X").unwrap_err()),
+        "Invalid subfield value."
+    );
+
+    assert_eq!(
+        format!("{}", Subfield::new('0', "12283\x1f643X").unwrap_err()),
+        "Invalid subfield value."
+    );
+}
+
+#[test]
+fn occurrence_new() {
+    assert!(Occurrence::new("01").is_ok());
+    assert!(Occurrence::new("001").is_ok());
+
+    assert_eq!(
+        format!("{}", Occurrence::new("0001").unwrap_err()),
+        "length < 2 || length > 3"
+    );
+
+    assert_eq!(
+        format!("{}", Occurrence::new("0a").unwrap_err()),
+        "Invalid occurrence '0a'"
+    );
+}
+
+#[test]
+fn field_new() {
+    assert!(Field::new("003@", None, vec![]).is_ok());
+
+    assert_eq!(
+        format!("{}", Field::new("303@", None, vec![]).unwrap_err()),
+        "Invalid field tag."
+    );
 }
