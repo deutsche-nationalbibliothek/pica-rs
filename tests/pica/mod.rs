8173b8aec0d566121366be9fb27c0ffa5a945cdf
Nico Wagner <nwagner84@protonmail.com>
2021-09-03 15:17:56 +0200

    Add `IO Error` test

--- tests/pica/mod.rs
+++ tests/pica/mod.rs
@@ -1,3 +1,7 @@
+use crate::TestResult;
+use assert_cmd::Command;
+use predicates::prelude::*;
+
 mod cat;
 mod completion;
 mod filter;
@@ -10,3 +14,23 @@ mod sample;
 mod select;
 mod slice;
 mod split;
+
+#[test]
+fn pica_io_error() -> TestResult {
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("--config")
+        .arg("/root/Pica.toml")
+        .arg("cat")
+        .arg("--skip-invalid")
+        .arg("tests/data/invalid.dat")
+        .assert();
+
+    assert
+        .failure()
+        .code(1)
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::starts_with("IO Error"));
+
+    Ok(())
+}
