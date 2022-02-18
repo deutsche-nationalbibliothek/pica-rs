5a77b8ce6cadafb0dd740ab11685d2a12808ca54
Nico Wagner <nwagner84@protonmail.com>
2022-02-18 04:55:11 +0100

    Add completions for `Elvish` and `Powershell`

--- src/bin/pica/cmds/completions.rs
+++ src/bin/pica/cmds/completions.rs
@@ -1,15 +1,23 @@
-use crate::util::{CliArgs, CliResult, Command};
-use clap::Arg;
-use clap_complete::{generate, Shell};
 use std::fs::File;
 use std::io::{self, Write};
+
+use clap::Arg;
+use clap_complete::{generate, Shell};
+
+use crate::util::{CliArgs, CliResult, Command};
 
 pub(crate) fn cli() -> Command {
     Command::new("completions")
         .about("Generate a completions file for Bash, Fish or ZSH shell.")
         .arg(
             Arg::new("shell")
-                .possible_values(&["fish", "bash", "zsh"])
+                .possible_values(&[
+                    "bash",
+                    "evlish",
+                    "fish",
+                    "powershell",
+                    "zsh",
+                ])
                 .required(true),
         )
         .arg(
@@ -29,10 +37,13 @@ pub(crate) fn run(args: &CliArgs, cli: &mut Command) -> CliResult<()> {
 
     match args.value_of("shell").unwrap() {
         "bash" => generate(Shell::Bash, cli, "pica", &mut writer),
+        "evlish" => generate(Shell::Elvish, cli, "pica", &mut writer),
         "fish" => generate(Shell::Fish, cli, "pica", &mut writer),
+        "powershell" => generate(Shell::PowerShell, cli, "pica", &mut writer),
         "zsh" => generate(Shell::Zsh, cli, "pica", &mut writer),
         _ => unreachable!(),
     }
 
+    writer.flush()?;
     Ok(())
 }
