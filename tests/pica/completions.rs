3650c3e2e2d21dadf975c43d1ce5ec9e488a7fbf
Nico Wagner <nwagner84@protonmail.com>
2022-02-18 05:08:38 +0100

    Add completions for `Elvish` and `Powershell` (#414)

--- tests/pica/completions.rs
+++ tests/pica/completions.rs
@@ -29,6 +29,30 @@ fn pica_bash_completion() -> TestResult {
 }
 
 #[test]
+fn pica_evlish_completion() -> TestResult {
+    let filename = Builder::new().tempfile()?;
+    let filename_str = filename.path();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("completions")
+        .arg("evlish")
+        .arg("--output")
+        .arg(filename_str)
+        .assert();
+    assert.success();
+    assert!(predicates::path::is_file().eval(filename_str));
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd.arg("completions").arg("evlish").assert();
+    assert
+        .success()
+        .stdout(predicate::path::eq_file(filename_str));
+
+    Ok(())
+}
+
+#[test]
 fn pica_fish_completion() -> TestResult {
     let filename = Builder::new().tempfile()?;
     let filename_str = filename.path();
@@ -45,6 +69,30 @@ fn pica_fish_completion() -> TestResult {
 
     let mut cmd = Command::cargo_bin("pica")?;
     let assert = cmd.arg("completions").arg("fish").assert();
+    assert
+        .success()
+        .stdout(predicate::path::eq_file(filename_str));
+
+    Ok(())
+}
+
+#[test]
+fn pica_powershell_completion() -> TestResult {
+    let filename = Builder::new().tempfile()?;
+    let filename_str = filename.path();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("completions")
+        .arg("powershell")
+        .arg("--output")
+        .arg(filename_str)
+        .assert();
+    assert.success();
+    assert!(predicates::path::is_file().eval(filename_str));
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd.arg("completions").arg("powershell").assert();
     assert
         .success()
         .stdout(predicate::path::eq_file(filename_str));
