953c03ed7100b9b14c9a95c68d39317aac251c68
Nico Wagner <nwagner84@protonmail.com>
2022-04-30 10:24:16 +0200

    Update docs

--- pica-core/src/lib.rs
+++ pica-core/src/lib.rs
@@ -1,2 +1,5 @@
-/// Parser result.
+/// Holds the result of a parsing function.
+///
+/// It takes a byte slice as input and uses `nom::Err<()>` as error variant. The
+/// type only depends the output type `O`.
 pub type ParseResult<'a, O> = Result<(&'a [u8], O), nom::Err<()>>;
