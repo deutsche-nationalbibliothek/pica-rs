076ba80605be91e90247b311e6d3f2097ee94f34
Nico Wagner <nwagner84@protonmail.com>
2022-09-17 17:57:38 +0200

    Refactor subfield

--- pica-record/benches/main.rs
+++ pica-record/benches/main.rs
@@ -1,7 +1,8 @@
 use criterion::{
     black_box, criterion_group, criterion_main, Criterion,
 };
-use pica_record::{FieldRef, OccurrenceRef, SubfieldRef, TagRef};
+use pica_record::{Subfield, SubfieldRef};
+// use pica_record::{FieldRef, OccurrenceRef, SubfieldRef, TagRef};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("SubfieldRef::from_bytes", |b| {
@@ -10,19 +11,23 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("TagRef::from_bytes", |b| {
-        b.iter(|| TagRef::from_bytes(black_box(b"003@")))
+    c.bench_function("Subfield::from_bytes", |b| {
+        b.iter(|| Subfield::from_bytes(black_box(b"\x1f0123456789X")))
     });
 
-    c.bench_function("OccurrenceRef::from_bytes", |b| {
-        b.iter(|| OccurrenceRef::from_bytes(black_box(b"/001")))
-    });
+    // c.bench_function("TagRef::from_bytes", |b| {
+    //     b.iter(|| TagRef::from_bytes(black_box(b"003@")))
+    // });
 
-    c.bench_function("FieldRef::from_bytes", |b| {
-        b.iter(|| {
-            FieldRef::from_bytes(black_box(b"003@ \x1f0123456789X\x1e"))
-        })
-    });
+    // c.bench_function("OccurrenceRef::from_bytes", |b| {
+    //     b.iter(|| OccurrenceRef::from_bytes(black_box(b"/001")))
+    // });
+
+    // c.bench_function("FieldRef::from_bytes", |b| {
+    //     b.iter(|| {
+    //         FieldRef::from_bytes(black_box(b"003@
+    // \x1f0123456789X\x1e"))     })
+    // });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- pica-record/src/lib.rs
+++ pica-record/src/lib.rs
@@ -2,29 +2,29 @@
 //! bibliographic records encoded in PICA+. There exists a read-only
 //! (immutable) and mutable variant of each primitive.
 
-#[cfg(test)]
-#[macro_use(quickcheck)]
-extern crate quickcheck_macros;
+// #[cfg(test)]
+// #[macro_use(quickcheck)]
+// extern crate quickcheck_macros;
 
-mod arbitrary;
+// mod arbitrary;
 mod error;
-mod field;
-mod occurrence;
+// mod field;
+// mod occurrence;
 mod subfield;
-mod tag;
+// mod tag;
 
 pub use error::ParsePicaError;
-pub use field::{Field, FieldRef};
-pub use occurrence::{Occurrence, OccurrenceRef};
-pub use subfield::{Subfield, SubfieldRef};
-pub use tag::{Tag, TagRef};
+// pub use field::{Field, FieldRef};
+// pub use occurrence::{Occurrence, OccurrenceRef};
+pub use subfield::{Subfield, SubfieldMut, SubfieldRef};
+// pub use tag::{Tag, TagRef};
 
 /// Parsers recognizing low-level primitives (e.g. subfield codes).
 #[rustfmt::skip]
 pub mod parser {
     pub(crate) const RS: u8 = b'\x1E'; // Record Separator
     pub(crate) const US: u8 = b'\x1F'; // Unit Separator
-    pub(crate) const SP: u8 = b' ';    // Space
+    // pub(crate) const SP: u8 = b' ';    // Space
 
     /// Holds the result of a parsing function.
     ///
@@ -32,10 +32,10 @@ pub mod parser {
     /// variant. The type only depends the output type `O`.
     pub type ParseResult<'a, O> = Result<(&'a [u8], O), nom::Err<()>>;
 
-    pub use super::field::parse_field_ref;
-    pub use super::occurrence::parse_occurrence_ref;
-    pub use super::occurrence::parse_occurrence_digits;
+    // pub use super::field::parse_field_ref;
+    // pub use super::occurrence::parse_occurrence_ref;
+    // pub use super::occurrence::parse_occurrence_digits;
     pub use super::subfield::parse_subfield_code;
     pub use super::subfield::parse_subfield_value;
-    pub use super::tag::parse_tag_ref;
+    // pub use super::tag::parse_tag_ref;
 }
