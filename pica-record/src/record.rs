a29abb8c88896812ec2337f90f118f0b82cc729a
Nico Wagner <n.wagner@dnb.de>
2022-09-21 09:35:17 +0200

    Add `is_empty` and `iter` functions

--- pica-record/src/record.rs
+++ pica-record/src/record.rs
@@ -49,7 +49,49 @@ impl<'a, T: AsRef<[u8]> + From<&'a BStr> + Display> Record<T> {
         Self(fields)
     }
 
+    /// Returns an iterator over the fields of the record.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if a parameter is invalid.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use pica_record::RecordRef;
+    ///
+    /// # fn main() { example().unwrap(); }
+    /// fn example() -> anyhow::Result<()> {
+    ///     let record = RecordRef::new(vec![
+    ///         ("003@", None, vec![('0', "123456789X")]),
+    ///         ("002@", None, vec![('0', "Oaf")]),
+    ///     ]);
+    ///
+    ///     assert_eq!(record.iter().len(), 2);
+    ///     Ok(())
+    /// }
+    /// ```
     pub fn iter(&self) -> Iter<Field<T>> {
         self.0.iter()
+    }
+
+    /// Returns `true` if the record contains no fields, otherwise
+    /// `false`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use pica_record::RecordRef;
+    ///
+    /// # fn main() { example().unwrap(); }
+    /// fn example() -> anyhow::Result<()> {
+    ///     let record =
+    ///         RecordRef::new(vec![("002@", None, vec![('0', "Oaf")])]);
+    ///     assert!(!record.is_empty());
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn is_empty(&self) -> bool {
+        self.0.len() == 0
     }
 }
