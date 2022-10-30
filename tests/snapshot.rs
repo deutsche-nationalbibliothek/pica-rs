5f5432f0953c00846410c348b13314ff75fc5803
Nico Wagner <nwagner84@protonmail.com>
2022-10-30 08:42:33 +0100

    Add `000-cat-multiple-files-output` test

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -1,6 +1,8 @@
 #[test]
-fn invalid() {
+fn snapshots() {
     trycmd::TestCases::new()
+        .case("tests/snapshot/cat/*.trycmd")
+        .case("tests/snapshot/cat/*.toml")
         .case("tests/snapshot/invalid/*.trycmd")
         .case("tests/snapshot/invalid/*.toml");
 }
