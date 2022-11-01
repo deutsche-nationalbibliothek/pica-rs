e95949a3cbf5da7e622f184eef6fa1e0608b8826
Nico Wagner <n.wagner@dnb.de>
2022-11-01 10:26:26 +0100

    Stabilize `completions` command (#538)

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -3,6 +3,8 @@ fn snapshots() {
     trycmd::TestCases::new()
         .case("tests/snapshot/cat/*.trycmd")
         .case("tests/snapshot/cat/*.toml")
+        .case("tests/snapshot/completions/*.trycmd")
+        .case("tests/snapshot/completions/*.toml")
         .case("tests/snapshot/invalid/*.trycmd")
         .case("tests/snapshot/invalid/*.toml");
 }
