bed7eaf8608c1bccc8adf591ee607aed9a091080
Nico Wagner <niko2342@nicowagner.me>
2023-02-06 18:16:08 +0100

    Add snapshot tests

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -1,16 +1,18 @@
 #[test]
 fn snapshots() {
     trycmd::TestCases::new()
-        .case("tests/snapshot/cat/*.trycmd")
         .case("tests/snapshot/cat/*.toml")
-        .case("tests/snapshot/filter/*.trycmd")
-        .case("tests/snapshot/filter/*.toml")
-        .case("tests/snapshot/frequency/*.trycmd")
-        .case("tests/snapshot/frequency/*.toml")
-        .case("tests/snapshot/count/*.trycmd")
-        .case("tests/snapshot/count/*.toml")
-        .case("tests/snapshot/completions/*.trycmd")
+        .case("tests/snapshot/cat/*.trycmd")
         .case("tests/snapshot/completions/*.toml")
+        .case("tests/snapshot/completions/*.trycmd")
+        .case("tests/snapshot/count/*.toml")
+        .case("tests/snapshot/count/*.trycmd")
+        .case("tests/snapshot/filter/*.toml")
+        .case("tests/snapshot/filter/*.trycmd")
+        .case("tests/snapshot/frequency/*.toml")
+        .case("tests/snapshot/frequency/*.trycmd")
+        .case("tests/snapshot/invalid/*.toml")
         .case("tests/snapshot/invalid/*.trycmd")
-        .case("tests/snapshot/invalid/*.toml");
+        .case("tests/snapshot/slice/*.toml")
+        .case("tests/snapshot/slice/*.trycmd");
 }
