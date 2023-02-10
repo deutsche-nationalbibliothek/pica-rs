cf24311fd8d587e7b0338d5333512b4962ffe958
Nico Wagner <niko2342@nicowagner.me>
2023-02-10 18:23:03 +0100

    Split test function

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -1,27 +1,62 @@
 #[test]
-fn snapshots() {
+fn cat() {
     trycmd::TestCases::new()
         .case("tests/snapshot/cat/*.toml")
-        .case("tests/snapshot/cat/*.trycmd")
-        .case("tests/snapshot/completions/*.toml")
-        .case("tests/snapshot/completions/*.trycmd")
-        .case("tests/snapshot/count/*.toml")
-        .case("tests/snapshot/count/*.trycmd")
-        .case("tests/snapshot/frequency/*.toml")
-        .case("tests/snapshot/frequency/*.trycmd")
-        .case("tests/snapshot/invalid/*.toml")
-        .case("tests/snapshot/invalid/*.trycmd")
-        .case("tests/snapshot/select/*.toml")
-        .case("tests/snapshot/select/*.trycmd")
-        .case("tests/snapshot/slice/*.toml")
-        .case("tests/snapshot/slice/*.trycmd")
-        .case("tests/snapshot/split/*.toml")
-        .case("tests/snapshot/split/*.trycmd");
+        .case("tests/snapshot/cat/*.trycmd");
 }
 
 #[test]
-fn filter_snapshots() {
+fn completions() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/completions/*.toml")
+        .case("tests/snapshot/completions/*.trycmd");
+}
+
+#[test]
+fn count() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/count/*.toml")
+        .case("tests/snapshot/count/*.trycmd");
+}
+
+#[test]
+fn filter() {
     trycmd::TestCases::new()
         .case("tests/snapshot/filter/*.toml")
         .case("tests/snapshot/filter/*.trycmd");
+}
+
+#[test]
+fn frequency() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/frequency/*.toml")
+        .case("tests/snapshot/frequency/*.trycmd");
+}
+
+#[test]
+fn invalid() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/invalid/*.toml")
+        .case("tests/snapshot/invalid/*.trycmd");
+}
+
+#[test]
+fn select() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/select/*.toml")
+        .case("tests/snapshot/select/*.trycmd");
+}
+
+#[test]
+fn slice() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/slice/*.toml")
+        .case("tests/snapshot/slice/*.trycmd");
+}
+
+#[test]
+fn split() {
+    trycmd::TestCases::new()
+        .case("tests/snapshot/split/*.toml")
+        .case("tests/snapshot/split/*.trycmd");
 }
