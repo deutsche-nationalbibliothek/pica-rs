65f95a13cc79c227f9cbffb9b8f2d7ef3b756dd8
Nico Wagner <n.wagner@dnb.de>
2023-08-29 14:42:24 +0200

    Cleanup / options

--- pica-toolkit/src/commands/explode.rs
+++ pica-toolkit/src/commands/explode.rs
@@ -1,12 +1,18 @@
 use std::ffi::OsString;
+use std::io;
 
-use clap::Parser;
-use pica_record::io::{ReaderBuilder, RecordsIterator, WriterBuilder};
+use clap::{value_parser, Parser};
+use pica_matcher::{MatcherOptions, RecordMatcher};
+use pica_record::io::{
+    ByteRecordWrite, ReaderBuilder, RecordsIterator, WriterBuilder,
+};
 use pica_record::{ByteRecord, Level};
 use serde::{Deserialize, Serialize};
 
 use crate::config::Config;
+use crate::progress::Progress;
 use crate::skip_invalid_flag;
+use crate::translit::translit_maybe2;
 use crate::util::CliResult;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -21,6 +27,46 @@ pub(crate) struct Explode {
     /// Skip invalid records that can't be decoded.
     #[arg(short, long)]
     skip_invalid: bool,
+
+    /// A filter expression used for searching
+    #[arg(long = "where")]
+    filter: Option<String>,
+
+    /// Connects the where clause with additional expressions using the
+    /// logical AND-operator (conjunction)
+    ///
+    /// This option can't be combined with `--or` or `--not`.
+    #[arg(long, requires = "filter", conflicts_with_all = ["or", "not"])]
+    and: Vec<String>,
+
+    /// Connects the where clause with additional expressions using the
+    /// logical OR-operator (disjunction)
+    ///
+    /// This option can't be combined with `--and` or `--not`.
+    #[arg(long, requires = "filter", conflicts_with_all = ["and", "not"])]
+    or: Vec<String>,
+
+    /// Connects the where clause with additional expressions using the
+    /// logical NOT-operator (negation)
+    ///
+    /// This option can't be combined with `--and` or `--or`.
+    #[arg(long, requires = "filter", conflicts_with_all = ["and", "or"])]
+    not: Vec<String>,
+
+    /// When this flag is provided, comparison operations will be
+    /// search case insensitive
+    #[arg(long, short)]
+    ignore_case: bool,
+
+    /// The minimum score for string similarity comparisons
+    /// (range: 0.0..1.0)
+    #[arg(long, value_parser = value_parser!(u8).range(0..100),
+        default_value = "75")]
+    strsim_threshold: u8,
+
+    /// Show progress bar (requires `-o`/`--output`).
+    #[arg(short, long, requires = "output")]
+    progress: bool,
 
     /// Write output to <OUTPUT> instead of stdout
     #[arg(short, long)]
@@ -71,6 +117,109 @@ macro_rules! push_record {
     };
 }
 
+fn process_main(
+    record: &ByteRecord,
+    matcher: Option<&RecordMatcher>,
+    options: &MatcherOptions,
+    writer: &mut Box<dyn ByteRecordWrite>,
+) -> io::Result<()> {
+    if let Some(matcher) = matcher {
+        if !matcher.is_match(&record, options) {
+            return Ok(());
+        }
+    }
+
+    writer.write_byte_record(&record)
+}
+
+fn process_copy(
+    record: &ByteRecord,
+    matcher: Option<&RecordMatcher>,
+    options: &MatcherOptions,
+    writer: &mut Box<dyn ByteRecordWrite>,
+) -> io::Result<()> {
+    let mut main = vec![];
+    let mut acc = vec![];
+    let mut records = vec![];
+    let mut local = None;
+    let mut count = None;
+
+    for field in record.iter() {
+        match field.level() {
+            Level::Main => main.push(field),
+            Level::Local => {
+                push_record!(records, main, local, acc);
+                local = Some(field);
+                count = None;
+            }
+            Level::Copy => {
+                if count != field.occurrence() {
+                    push_record!(records, main, local, acc);
+                    count = field.occurrence();
+                }
+
+                acc.push(field);
+            }
+        }
+    }
+
+    push_record!(records, main, local, acc);
+    for fields in records {
+        let data = record_bytes!(fields);
+        let record =
+            ByteRecord::from_bytes(&data).expect("valid record");
+
+        if let Some(matcher) = matcher {
+            if !matcher.is_match(&record, options) {
+                continue;
+            }
+        }
+
+        writer.write_byte_record(&record)?;
+    }
+
+    Ok(())
+}
+
+fn process_local(
+    record: &ByteRecord,
+    matcher: Option<&RecordMatcher>,
+    options: &MatcherOptions,
+    writer: &mut Box<dyn ByteRecordWrite>,
+) -> io::Result<()> {
+    let mut main = vec![];
+    let mut acc = vec![];
+    let mut records = vec![];
+
+    for field in record.iter() {
+        match field.level() {
+            Level::Main => main.push(field),
+            Level::Copy => acc.push(field),
+            Level::Local => {
+                push_record!(records, main, acc);
+                acc.push(field)
+            }
+        }
+    }
+
+    push_record!(records, main, acc);
+
+    for fields in records.iter() {
+        let data = record_bytes!(fields);
+        let record = ByteRecord::from_bytes(&data).unwrap();
+
+        if let Some(matcher) = matcher {
+            if !matcher.is_match(&record, options) {
+                continue;
+            }
+        }
+
+        writer.write_byte_record(&record)?;
+    }
+
+    Ok(())
+}
+
 impl Explode {
     pub(crate) fn run(self, config: &Config) -> CliResult<()> {
         let skip_invalid = skip_invalid_flag!(
@@ -79,8 +228,60 @@ impl Explode {
             config.global
         );
 
+        let options = MatcherOptions::new()
+            .strsim_threshold(self.strsim_threshold as f64 / 100.0)
+            .case_ignore(self.ignore_case);
+
+        let translit = if let Some(ref global) = config.global {
+            global.translit
+        } else {
+            None
+        };
+
+        let matcher = if let Some(matcher_str) = self.filter {
+            let mut matcher = RecordMatcher::new(&translit_maybe2(
+                &matcher_str,
+                translit,
+            ))?;
+
+            for matcher_str in self.and.iter() {
+                matcher = matcher
+                    & RecordMatcher::new(&translit_maybe2(
+                        matcher_str,
+                        translit,
+                    ))?;
+            }
+
+            for matcher_str in self.or.iter() {
+                matcher = matcher
+                    | RecordMatcher::new(&translit_maybe2(
+                        matcher_str,
+                        translit,
+                    ))?;
+            }
+
+            for matcher_str in self.not.iter() {
+                matcher = matcher
+                    & !RecordMatcher::new(&translit_maybe2(
+                        matcher_str,
+                        translit,
+                    ))?;
+            }
+
+            Some(matcher)
+        } else {
+            None
+        };
+
+        let mut progress = Progress::new(self.progress);
         let mut writer =
             WriterBuilder::new().from_path_or_stdout(self.output)?;
+
+        let process_record = match self.level {
+            Level::Main => process_main,
+            Level::Copy => process_copy,
+            Level::Local => process_local,
+        };
 
         for filename in self.filenames {
             let mut reader =
@@ -90,92 +291,28 @@ impl Explode {
                 match result {
                     Err(e) => {
                         if e.is_invalid_record() && skip_invalid {
+                            progress.invalid();
                             continue;
                         } else {
                             return Err(e.into());
                         }
                     }
-                    Ok(record) => match self.level {
-                        Level::Main => {
-                            writer.write_byte_record(&record)?
-                        }
-                        Level::Copy => {
-                            let mut main = vec![];
-                            let mut acc = vec![];
-                            let mut records = vec![];
-                            let mut local = None;
-                            let mut count = None;
-
-                            for field in record.iter() {
-                                match field.level() {
-                                    Level::Main => main.push(field),
-                                    Level::Local => {
-                                        push_record!(
-                                            records, main, local, acc
-                                        );
-
-                                        local = Some(field);
-                                        count = None;
-                                    }
-                                    Level::Copy => {
-                                        if count != field.occurrence() {
-                                            push_record!(
-                                                records, main, local,
-                                                acc
-                                            );
-
-                                            count = field.occurrence();
-                                        }
-
-                                        acc.push(field);
-                                    }
-                                }
-                            }
-
-                            push_record!(records, main, local, acc);
-
-                            for fields in records {
-                                let data = record_bytes!(fields);
-                                let record =
-                                    ByteRecord::from_bytes(&data)
-                                        .expect("valid record");
-                                writer.write_byte_record(&record)?;
-                            }
-                        }
-                        Level::Local => {
-                            let mut main = vec![];
-                            let mut acc = vec![];
-                            let mut records = vec![];
-
-                            for field in record.iter() {
-                                match field.level() {
-                                    Level::Main => main.push(field),
-                                    Level::Copy => acc.push(field),
-                                    Level::Local => {
-                                        push_record!(
-                                            records, main, acc
-                                        );
-                                        acc.push(field)
-                                    }
-                                }
-                            }
-
-                            push_record!(records, main, acc);
-
-                            for fields in records.iter() {
-                                let data = record_bytes!(fields);
-                                let record =
-                                    ByteRecord::from_bytes(&data)
-                                        .unwrap();
-                                writer.write_byte_record(&record)?;
-                            }
-                        }
-                    },
+                    Ok(record) => {
+                        progress.record();
+                        process_record(
+                            &record,
+                            matcher.as_ref(),
+                            &options,
+                            &mut writer,
+                        )?;
+                    }
                 }
             }
         }
 
+        progress.record();
         writer.finish()?;
+
         Ok(())
     }
 }
