02acbc090e31a761554d529dee903f1c3673e2c8
Nico Wagner <niko2342@nicowagner.me>
2023-11-19 21:52:23 +0100

    Add crate docs

--- crates/pica-record/src/lib.rs
+++ crates/pica-record/src/lib.rs
@@ -1,5 +1,27 @@
 //! This crate provides the low-level primitives to work with
 //! bibliographic records encoded in PICA+.
+//!
+//! There exists an immutable and a mutable variant for each primitve.
+//! The immutable variant is used to parse the corresponding component
+//! of a record, without owning the data. This type is mostly a wrapper
+//! of the underlying data (byte slices). On the other hand there is
+//! also a mutable variant, which is mostly used in upstream crates.
+//! This variant owns it's data.
+//!
+//! This crate also provides two higher-level data structure to work
+//! with records: [`ByteRecord`] and [`StringRecord`]. The first type
+//! is a wrapper of a [`RecordRef`] and provides more functions to work
+//! with records as well as an mechanism to cache the complete byte
+//! sequence of the original record. This improves the performance when
+//! the record is written back to a stream. It is important to note that
+//! a [`ByteRecord`] might contain invalid UTF-8 data. When it's
+//! important to guarantee valid UTF-8 data, use a [`StringRecord`]
+//! instead.
+//!
+//! Finally, the [`io`] module provides utilities for reading and
+//! writing PICA+ records and the [`parser`] module exposes the internal
+//! parse combinators, which are used in upstream crates (matcher,
+//! select).
 
 mod error;
 mod field;
@@ -15,7 +37,7 @@ pub use field::{Field, FieldRef};
 pub use level::Level;
 pub use occurrence::{Occurrence, OccurrenceRef};
 pub use record::{ByteRecord, Record, RecordRef, StringRecord};
-pub use subfield::SubfieldRef;
+pub use subfield::{Subfield, SubfieldRef};
 pub use tag::{Tag, TagRef};
 
 /// Parsers recognizing low-level primitives (e.g. subfield codes).
