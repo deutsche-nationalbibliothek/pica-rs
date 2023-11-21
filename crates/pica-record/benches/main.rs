dbb6d604264d87b70747079f901c2fbc3f95bd52
Nico Wagner <n.wagner@dnb.de>
2023-11-21 21:05:37 +0100

    Revise `pica-record` benchmarks (#713)

--- crates/pica-record/benches/main.rs
+++ crates/pica-record/benches/main.rs
@@ -1,65 +1,37 @@
-use criterion::{
-    black_box, criterion_group, criterion_main, Criterion,
-};
-use pica_record::{
-    FieldMut, FieldRef, OccurrenceMut, OccurrenceRef, RecordMut,
-    RecordRef, SubfieldMut, SubfieldRef, TagMut, TagRef,
-};
+use std::path::Path;
+use std::sync::OnceLock;
+use std::{env, fs};
+
+use criterion::*;
+use pica_record::{Record, RecordRef};
+use quickcheck::{Arbitrary, Gen};
+
+fn ada_lovelace() -> &'static [u8] {
+    static DATA: OnceLock<Vec<u8>> = OnceLock::new();
+    DATA.get_or_init(|| {
+        let path = Path::new(&env!("CARGO_MANIFEST_DIR"))
+            .join("../../tests/data/ada.dat");
+        fs::read_to_string(&path).unwrap().as_bytes().to_vec()
+    })
+}
+
+fn arbitrary(size: usize) -> Vec<u8> {
+    let record = Record::arbitrary(&mut Gen::new(size));
+    let mut bytes = vec![];
+    let _ = record.write_to(&mut bytes);
+    bytes
+}
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("SubfieldRef::from_bytes", |b| {
+    c.bench_function("RecordRef::from_bytes (ada)", |b| {
+        b.iter(|| RecordRef::from_bytes(black_box(ada_lovelace())))
+    });
+
+    c.bench_function("RecordRef::from_bytes (arbitrary)", |b| {
+        let bytes = arbitrary(100);
         b.iter(|| {
-            SubfieldRef::from_bytes(black_box(b"\x1f0123456789X"))
-        })
-    });
-
-    c.bench_function("SubfieldMut::from_bytes", |b| {
-        b.iter(|| {
-            SubfieldMut::from_bytes(black_box(b"\x1f0123456789X"))
-        })
-    });
-
-    c.bench_function("TagRef::from_bytes", |b| {
-        b.iter(|| TagRef::from_bytes(black_box(b"003@")))
-    });
-
-    c.bench_function("TagMut::from_bytes", |b| {
-        b.iter(|| TagMut::from_bytes(black_box(b"003@")))
-    });
-
-    c.bench_function("OccurrenceRef::from_bytes", |b| {
-        b.iter(|| OccurrenceRef::from_bytes(black_box(b"/001")))
-    });
-
-    c.bench_function("OccurrenceMut::from_bytes", |b| {
-        b.iter(|| OccurrenceMut::from_bytes(black_box(b"/001")))
-    });
-
-    c.bench_function("FieldRef::from_bytes", |b| {
-        b.iter(|| {
-            FieldRef::from_bytes(black_box(b"003@ \x1f0123456789X\x1e"))
-        })
-    });
-
-    c.bench_function("FieldMut::from_bytes", |b| {
-        b.iter(|| {
-            FieldMut::from_bytes(black_box(b"003@ \x1f0123456789X\x1e"))
-        })
-    });
-
-    c.bench_function("RecordRef::from_bytes", |b| {
-        b.iter(|| {
-            RecordRef::from_bytes(black_box(
-                b"003@ \x1f0123456789X\x1e002@ \x1f0Olfo\x1e\n",
-            ))
-        })
-    });
-
-    c.bench_function("RecordMut::from_bytes", |b| {
-        b.iter(|| {
-            RecordMut::from_bytes(black_box(
-                b"003@ \x1f0123456789X\x1e002@ \x1f0Olfo\x1e\n",
-            ))
+            let _record =
+                RecordRef::from_bytes(black_box(&bytes)).unwrap();
         })
     });
 }
