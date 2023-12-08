cabe8b3fac64d7e60ffbb0faeb070347406c22b0
Nico Wagner <niko2342@nicowagner.me>
2023-12-08 10:48:54 +0100

    Add `Query` fuzz test

--- crates/pica-select/fuzz/fuzz_targets/fuzz_query.rs
+++ crates/pica-select/fuzz/fuzz_targets/fuzz_query.rs
@@ -1,12 +1,8 @@
 #![no_main]
 
-use std::str::FromStr;
-
 use libfuzzer_sys::fuzz_target;
 use pica_select::Query;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        let _ = Query::from_str(s);
-    }
+    let _ = Query::try_from(data);
 });
