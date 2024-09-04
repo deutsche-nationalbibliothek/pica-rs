a3bdff1ddaf9424e02a00c1f00075e4be2980de6
Nico Wagner <n.wagner@dnb.de>
2024-09-04 08:08:18 +0000

    Move `SubfieldValue` into `primitives` module
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- pica-record/src/primitives/mod.rs
+++ pica-record/src/primitives/mod.rs
@@ -1,5 +1,7 @@
 pub use code::SubfieldCode;
-pub use parse::parse_subfield_code;
+pub use parse::{parse_subfield_code, parse_subfield_value_ref};
+pub use value::{SubfieldValue, SubfieldValueRef};
 
 mod code;
 mod parse;
+mod value;
