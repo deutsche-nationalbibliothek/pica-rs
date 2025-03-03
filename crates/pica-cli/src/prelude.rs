d67ca8819b58f97d1121cde8c0f707d5f7df19d2
Nico Wagner <n.wagner@dnb.de>
2025-03-03 09:29:29 +0000

    fix rustfmt
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/pica-cli/src/prelude.rs
+++ crates/pica-cli/src/prelude.rs
@@ -1,5 +1,5 @@
 pub(crate) use crate::config::Config;
-pub(crate) use crate::error::{bail, CliError, CliResult};
+pub(crate) use crate::error::{CliError, CliResult, bail};
 pub(crate) use crate::progress::Progress;
 pub(crate) use crate::translit::{NormalizationForm, translit};
 pub(crate) use crate::utils::{FilterSet, parse_predicates};
