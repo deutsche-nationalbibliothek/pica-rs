3b2dd484dd3a38a1589f91dcfddd7028ca0a40cd
Nico Wagner <n.wagner@dnb.de>
2025-03-20 13:23:43 +0100

    feat(check): add `isni` check (#925)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

---
+++ crates/pica-cli/tests/check/isni.rs
@@ -0,0 +1,338 @@
+use assert_cmd::Command;
+use assert_fs::TempDir;
+use assert_fs::prelude::*;
+
+use crate::prelude::*;
+
+#[test]
+fn orcid() -> TestResult {
+    let temp_dir = TempDir::new().unwrap();
+    let ruleset = temp_dir.child("rules.toml");
+    ruleset
+        .write_str(
+            r#"
+            [rule.R1]
+            check = "isni"
+            path = "028[AC]/*.y"
+        "#,
+        )
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000-0002-9079-5939\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R1,error,0000-0002-9079-5939\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000000290795939\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R1,error,0000000290795939\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000-0002-9079-593X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy000000029079593X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    temp_dir.close().unwrap();
+
+    Ok(())
+}
+
+#[test]
+fn isni() -> TestResult {
+    let temp_dir = TempDir::new().unwrap();
+    let ruleset = temp_dir.child("rules.toml");
+    ruleset
+        .write_str(
+            r#"
+            [rule.R2]
+            check = "isni"
+            path = "028[AC]/*.y"
+        "#,
+        )
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000-0001-2146-4389\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R2,error,0000-0001-2146-4389\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000000121464389\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R2,error,0000000121464389\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy000000012146438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000 0001 2146 438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy0000-0001-2146-438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    temp_dir.close().unwrap();
+
+    Ok(())
+}
+
+#[test]
+fn prefix() -> TestResult {
+    let temp_dir = TempDir::new().unwrap();
+    let ruleset = temp_dir.child("rules.toml");
+    ruleset
+        .write_str(
+            r#"
+            [rule.R3]
+            check = "isni"
+            path = "028[AC]/*.y"
+            prefix = "(isni)"
+        "#,
+        )
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)0000-0001-2146-4389\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R3,error,(isni)0000-0001-2146-4389\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)0000000121464389\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R3,error,(isni)0000000121464389\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)000000012146438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)0000 0001 2146 438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)0000-0001-2146-438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    temp_dir.close().unwrap();
+
+    Ok(())
+}
+
+#[test]
+fn options() -> TestResult {
+    let temp_dir = TempDir::new().unwrap();
+    let ruleset = temp_dir.child("rules.toml");
+    ruleset
+        .write_str(
+            r#"
+            [rule.R3]
+            check = "isni"
+            path = '028[AC]/*{ y | y =? "ISNI" }'
+            case-ignore = true
+            prefix = "(isni)"
+        "#,
+        )
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)0000-0001-2146-4389\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R3,error,(isni)0000-0001-2146-4389\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e028A \x1fy(isni)000000012146438X\x1e\n",
+        )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::is_empty());
+
+    temp_dir.close().unwrap();
+
+    Ok(())
+}
