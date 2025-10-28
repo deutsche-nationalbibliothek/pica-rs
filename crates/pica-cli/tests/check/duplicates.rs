222ade847578b385d02b8554b44532427b9f85ab
Nico Wagner <n.wagner@dnb.de>
2025-10-28 10:58:51 +0000

    update tests
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/pica-cli/tests/check/duplicates.rs
+++ crates/pica-cli/tests/check/duplicates.rs
@@ -80,7 +80,7 @@ fn check_duplicates_query() -> TestResult {
         .success()
         .code(0)
         .stdout(predicates::ord::eq(
-            "ppn,rule,level,message\n123456789X,R001,error,i-dnb\n",
+            "ppn,rule,level,message\n123456789X,R001,error,i|dnb\n",
         ))
         .stderr(predicates::str::is_empty());
 
@@ -143,7 +143,7 @@ fn check_duplicates_threshold() -> TestResult {
         .success()
         .code(0)
         .stdout(predicates::ord::eq(
-            "ppn,rule,level,message\n123456789X,R001,error,i-dnb\n",
+            "ppn,rule,level,message\n123456789X,R001,error,i|dnb\n",
         ))
         .stderr(predicates::str::is_empty());
 
@@ -180,6 +180,41 @@ fn check_duplicates_ignore_case() -> TestResult {
         .code(0)
         .stdout(predicates::ord::eq(
             "ppn,rule,level,message\n123456789X,R001,error,D63\n",
+        ))
+        .stderr(predicates::str::is_empty());
+
+    temp_dir.close().unwrap();
+    Ok(())
+}
+
+#[test]
+fn check_duplicates_separator() -> TestResult {
+    let temp_dir = TempDir::new().unwrap();
+    let ruleset = temp_dir.child("rules.toml");
+    ruleset
+        .write_str(
+            r#"
+            [rule.R001]
+            check = 'duplicates'
+            separator = '+'
+            query = '045E{ E, H }'
+        "#,
+        )
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("pica")?;
+    let assert = cmd
+        .arg("check")
+        .args(["-R", ruleset.to_str().unwrap()])
+        .write_stdin(
+            b"003@ \x1f0123456789X\x1e045E \x1fe100\x1fEi\x1fHdnb\x1e045E \x1fe110\x1fEi\x1fHdnb\x1e\n", )
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq(
+            "ppn,rule,level,message\n123456789X,R001,error,i+dnb\n",
         ))
         .stderr(predicates::str::is_empty());
 
